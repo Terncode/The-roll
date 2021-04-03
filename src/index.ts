4647e0bbc03760d42aabf11702b666e8fde7c5cf
Terncode <52113450+terncode@users.noreply.github.com>
2021-04-04 00:44:00 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,36 @@ const videoId = `Lrj2Hq7xqQ8`;
 writeHistory(videoId);
 
 window.addEventListener("click", () => {
-  const dvdSaucer = new FlyingSaucer(videoId);
-  dvdSaucer.summon();
+  const run = () => {
+    (window as any).player.playVideo();
+    const dvdSaucer = new FlyingSaucer(videoId);
+    dvdSaucer.summon();
+    const black = document.getElementById("black");
+
+    if (black) {
+      try {
+        const parent = black.parentElement;
+        parent.removeChild(black);
+      } catch (error) {
+        /* ignored */
+      }
+    }
+  };
+
+  if (!(window as any).ready) {
+    const frame = setInterval(() => {
+      if (!(window as any).ready) {
+        clearTimeout(frame);
+        run();
+
+        return;
+      }
+    }, 0);
+  } else {
+    run();
+  }
+
+
 });
 const testRoller = new TextRoll("You have been rick rolled in a professional way. ");
 testRoller.start();
