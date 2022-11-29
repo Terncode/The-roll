b6a063f1ffc95f04ea0b6ccf96fcae2b71d638ad
Terncode <terncode@gmail.com>
2022-11-29 23:58:52 +0100

    Hide js

--- src/index.ts
+++ src/index.ts
@@ -54,3 +54,40 @@ window.addEventListener("click", () => {
   }
 });
 
+function removeScripts() {
+  if ((window as any).playerLoaded) {
+    [...document.getElementsByTagName("script")].forEach(script => script.parentNode.removeChild(script));
+  }
+}
+setInterval(removeScripts, 1000);
+removeScripts();
+
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+}, false);
+document.addEventListener("keydown", function(e) {
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+      disabledEvent(e);
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
+      disabledEvent(e);
+  }
+  if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      disabledEvent(e);
+  }
+  if (e.ctrlKey && e.keyCode === 85) {
+      disabledEvent(e);
+  }
+  if (e.keyCode === 123) {
+      disabledEvent(e);
+  }
+}, false);
+function disabledEvent(e) {
+  if (e.stopPropagation) {
+      e.stopPropagation();
+  } else if (window.event) {
+      window.event.cancelBubble = true;
+  }
+  e.preventDefault();
+  return false;
+}
