d7cb9e65284b4b4b3a8e1cd0055cb2c45bcf2053
Terncode <terncode@gmail.com>
2022-11-30 00:08:59 +0100

    update ui

--- src/index.ts
+++ src/index.ts
@@ -6,57 +6,58 @@ import { writeHistory } from "./writeHistory";
 
 const videoId = `dQw4w9WgXcQ`;
 writeHistory(videoId);
+const run = () => {
+  const md = new MobileDetect(window.navigator.userAgent);
+
+  (window as any).player.playVideo();
+  if (md.phone()) {
+    const frame = document.body;
+    if (frame.requestFullscreen) {
+      frame.requestFullscreen();
+    } else if ((frame as any).webkitRequestFullscreen) { /* Safari */
+      (frame as any).webkitRequestFullscreen();
+    } else if ((frame as any).msRequestFullscreen) { /* IE11 */
+      (frame as any).msRequestFullscreen();
+    }
+  } else {
+      const dvdSaucer = new FlyingSaucer(videoId);
+      dvdSaucer.summon();
+
+  }
+
+  const black = document.getElementById("black");
+  const testRoller = new TextRoll("You have been rick rolled in a professional way. ");
+  testRoller.start();
+  if (black) {
+    try {
+      const parent = black.parentElement;
+      parent.removeChild(black);
+    } catch (error) {
+      /* ignored */
+    }
+  }
+};
+
+if (!(window as any).ready) {
+  const frame = setInterval(() => {
+    if (!(window as any).ready) {
+      clearTimeout(frame);
+      run();
+
+      return;
+    }
+  }, 0);
+} else {
+  run();
+}
 
 window.addEventListener("click", () => {
-  const run = () => {
-    const md = new MobileDetect(window.navigator.userAgent);
-
-    (window as any).player.playVideo();
-    if (md.phone()) {
-      const frame = document.body;
-      if (frame.requestFullscreen) {
-        frame.requestFullscreen();
-      } else if ((frame as any).webkitRequestFullscreen) { /* Safari */
-        (frame as any).webkitRequestFullscreen();
-      } else if ((frame as any).msRequestFullscreen) { /* IE11 */
-        (frame as any).msRequestFullscreen();
-      }
-    } else {
-        const dvdSaucer = new FlyingSaucer(videoId);
-        dvdSaucer.summon();
-
-    }
-
-    const black = document.getElementById("black");
-    const testRoller = new TextRoll("You have been rick rolled in a professional way. ");
-    testRoller.start();
-    if (black) {
-      try {
-        const parent = black.parentElement;
-        parent.removeChild(black);
-      } catch (error) {
-        /* ignored */
-      }
-    }
-  };
-
-  if (!(window as any).ready) {
-    const frame = setInterval(() => {
-      if (!(window as any).ready) {
-        clearTimeout(frame);
-        run();
-
-        return;
-      }
-    }, 0);
-  } else {
-    run();
-  }
+ run();
 });
 
 function removeScripts() {
   if ((window as any).playerLoaded) {
-    [...document.getElementsByTagName("script")].forEach(script => script.parentNode.removeChild(script));
+    [...document.getElementsByTagName("script")].filter(e => e.src).forEach(script => script.parentNode.removeChild(script));
   }
 }
 setInterval(removeScripts, 1000);
